4f5b8bb0b1c3f87066bb11aeb4fe341163c431a1
SymptomTracker <symptomtracker@localhost>
2026-08-09 06:42:19 +0200

    Initial SymptomTracker Flask application

---
+++ run.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from app import create_app
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(
+        host="127.0.0.1",
+        port=5060,
+        debug=True
+    )
